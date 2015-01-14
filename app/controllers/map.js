a6bc11056f84cb93f4a8f84a50a25b7ddf286a34
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2015-01-14 08:03:49 +0800

    try to handle maxmind errors

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -4,10 +4,12 @@ var MapController = Ember.Controller.extend({
 
   needs: ['maxmind', 'geolocation'],
 
-  loading: Ember.computed.alias('controllers.maxmind.isPending'),
+  // state
+  loaded: Ember.computed.alias('controllers.maxmind.isSettled').readOnly(),
+  succeeded: Ember.computed.alias('controllers.maxmind.isFulfilled').readOnly(),
 
+  // data
   maxmind: Ember.computed.alias('controllers.maxmind.model').readOnly(),
-
   latitude:  Ember.computed.alias('maxmind.location.latitude').readOnly(),
   longitude: Ember.computed.alias('maxmind.location.longitude').readOnly(),
   city: Ember.computed.alias('maxmind.city.names.en').readOnly(),
